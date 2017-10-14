5452d891b638997651418a52709a1d1cd703a629
ashiato45 <katiiijp@gmail.com>
2017-10-14 14:16:18 +0900

    solved 4-1 by modifying prefix

--- exercises/mutable_reference/src/main.rs
+++ exercises/mutable_reference/src/main.rs
@@ -9,19 +9,18 @@
 fn main() {
     let (mut word_a, word_b) = words();
     println!("単語1：{:?}\n単語2：{:?}", word_a, word_b);
-    let text = concat(&mut word_a, &word_b);
-    println!("結合された結果：{:?}", text);
+    concat(&mut word_a, &word_b);
+    println!("結合された結果：{:?}", word_a);
 }
 
 fn words() -> (String, String) {
     (format!("こんにちは"), format!("世界"))
 }
 
-fn concat(prefix: &mut String, postfix: &String) -> String {
+fn concat(prefix: &mut String, postfix: &String) {
     prefix.push(' ');
     for c in postfix.chars() {
         prefix.push(c);
     }
-    prefix.clone()
 }
 
